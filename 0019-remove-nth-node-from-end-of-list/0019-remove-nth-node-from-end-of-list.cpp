657a6277a2510fc0804075eae633fb9dddc8c8a0
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-16 12:34:44 +0530

    Time: 0 ms (100%), Space: 14.7 MB (34.86%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl; 
+#define REP(i,n) for(int i=0;i<(n);i++) 
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++) 
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--) 
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; } 
+
+const int INF = 1<<29; 
+typedef long long ll; 
+
+inline int two(int n) { return 1 << n; } 
+inline int test(int n, int b) { return (n>>b)&1; } 
+inline void set_bit(int & n, int b) { n |= two(b); } 
+inline void unset_bit(int & n, int b) { n &= ~two(b); } 
+inline int last_bit(int n) { return n & (-n); } 
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; } 
+
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); } 
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *fast = head, *slow = head;
+        int count = 0;
+        
+        while (count < n) {
+            fast = fast->next;
+            count++;
+        }
+        
+        return (fast == nullptr) ? head->next : removeNode(fast, slow, head);
+    }
+    
+private:
+    ListNode* removeNode(ListNode* fast, ListNode* slow, ListNode* head) {
+        while (fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return head;
+    }
+};
