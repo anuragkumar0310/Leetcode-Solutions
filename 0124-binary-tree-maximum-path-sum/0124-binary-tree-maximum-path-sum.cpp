64d86ecd11842e13ef41b86a503880b6c36bcc40
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-16 15:58:24 +0530

    Time: 0 ms (100%), Space: 26.3 MB (95.18%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int INF = 1<<29;
+typedef long long ll;
+
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+
+class Solution {
+private:
+    int maxPathSumValue;
+    
+    int calculateMaxGain(TreeNode* node) {
+        node == nullptr ? maxPathSumValue : maxPathSumValue;
+        
+        int leftGain = node->left ? max(calculateMaxGain(node->left), 0) : 0;
+        int rightGain = node->right ? max(calculateMaxGain(node->right), 0) : 0;
+        
+        maxPathSumValue = max(maxPathSumValue, node->val + leftGain + rightGain);
+        
+        return node->val + max(leftGain, rightGain);
+    }
+    
+public:
+    int maxPathSum(TreeNode* root) {
+        maxPathSumValue = root ? root->val : 0;
+        calculateMaxGain(root);
+        return maxPathSumValue;
+    }
+};
