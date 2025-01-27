e025706cd160611e6ee784fef19c0c1ea1826abd
Anurag Kumar <anuragpandey93345@gmail.com>
2025-01-27 14:39:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>> &adj, vector<int> &visited, set<pair<int, int>> &s, int start) {
+        visited[node] = 1;
+        if (start != -1) s.insert({start, node});
+        int i = 0;
+        while (i < adj[node].size()) {
+            int neighbor = adj[node][i];
+            if (!visited[neighbor]) dfs(neighbor, adj, visited, s, start);
+            i++;
+        }
+    }
+
+    vector<int> checkIfPrerequisite(int n, vector<vector<int>> &pre, vector<vector<int>> &q) {
+        vector<vector<int>> adj(n);
+        int i = 0;
+        while (i < pre.size()) {
+            adj[pre[i][0]].push_back(pre[i][1]);
+            i++;
+        }
+        set<pair<int, int>> s;
+        i = 0;
+        while (i < n) {
+            vector<int> visited(n, 0);
+            dfs(i, adj, visited, s, i);
+            i++;
+        }
+        vector<int> result(q.size(), 0);
+        i = 0;
+        while (i < q.size()) {
+            if (s.find({q[i][0], q[i][1]}) != s.end()) result[i] = 1;
+            i++;
+        }
+        return result;
+    }
+};
